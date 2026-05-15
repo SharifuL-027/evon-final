da3cd5123ece35b4093b39eae9a884dd5665d78f
Md Shariful Islam <shariful07cse@gmail.com>
2026-05-15 10:13:46 +0600

    again

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Navbar from './Component/Navbar';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <main className="pt-20 min-h-screen bg-gray-50">
+        <Routes>
+          {/* Static Routes */}
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} /> */}
+
+          {/* Dynamic Routes (Notice the :id) */}
+          {/* <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/checkout/:id" element={<OrderPage />} /> */}
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
